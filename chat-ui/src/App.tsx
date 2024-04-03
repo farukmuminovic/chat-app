51f9f0254f37bc3f913874f06db8816d881ada8b
farukmuminovic <faruk_muminovic@hotmail.com>
2024-04-04 00:32:09 +0200

    Implemented Apollo Client for communication with GraphQL server and logic on UI for signing up users Resolved problem with CORS by proxying to GQL server

--- chat-ui/src/App.tsx
+++ chat-ui/src/App.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import {
-  Container,
-  createTheme,
-  CssBaseline,
-  ThemeProvider,
+    Container,
+    createTheme,
+    CssBaseline,
+    ThemeProvider,
 } from "@mui/material";
-import { RouterProvider } from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import router from "./components/Routes";
+import client from "./constants/apollo-client";
+import {ApolloProvider} from "@apollo/client";
 
 const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
+    palette: {
+        mode: "dark",
+    },
 });
 const App = () => {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Container>
-        <RouterProvider router={router} />
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline/>
+                <Container>
+                    <RouterProvider router={router}/>
+                </Container>
+            </ThemeProvider>
+        </ApolloProvider>
+    );
 };
 
 export default App;
--- chat-ui/src/components/auth/SignUp.tsx
+++ chat-ui/src/components/auth/SignUp.tsx
@@ -1,15 +1,26 @@
 import Auth from "./Auth";
-import { Link } from "react-router-dom";
-import { Link as MUILink } from "@mui/material";
+import {Link} from "react-router-dom";
+import {Link as MUILink} from "@mui/material";
+import {useCreateUser} from "../../hooks/useCreateUser";
 
 const SignUp = () => {
-  return (
-    <Auth submitLabel={"Sign Up"} onSubmit={async () => {}}>
-      <Link to={"/login"} style={{ alignSelf: "center" }}>
-        <MUILink>Already registered? Log in</MUILink>
-      </Link>
-    </Auth>
-  );
+    const [createUser] = useCreateUser()
+    return (
+        <Auth submitLabel={"Sign Up"} onSubmit={async ({email, password}) => {
+            await createUser({
+                variables: {
+                    createUserInput: {
+                        email,
+                        password
+                    }
+                }
+            })
+        }}>
+            <Link to={"/login"} style={{alignSelf: "center"}}>
+                <MUILink>Already registered? Log in</MUILink>
+            </Link>
+        </Auth>
+    );
 };
 
 export default SignUp;
