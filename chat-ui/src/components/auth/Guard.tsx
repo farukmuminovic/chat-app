61385f9d6c216c2d2c2e379ae2bcb2e15474ccf5
farukmuminovic <faruk_muminovic@hotmail.com>
2024-05-12 21:20:23 +0200

    Implemented conditional rendering of chat list

--- chat-ui/src/components/auth/Guard.tsx
+++ chat-ui/src/components/auth/Guard.tsx
@@ -4,6 +4,7 @@ import {useEffect} from "react";
 import {authenticatedVar} from "../../constants/authenticated";
 import {snackVar} from "../../constants/snack";
 import {UNKNOWN_ERROR_SNACK_MESSAGE} from "../../constants/error";
+import {usePath} from "../../hooks/usePath";
 
 interface GuardProps {
     children: JSX.Element;
@@ -11,6 +12,8 @@ interface GuardProps {
 
 const Guard = ({children}: GuardProps) => {
     const {data: user, error} = useGetMe();
+    const {path} = usePath();
+
     useEffect(() => {
         if (user) {
             authenticatedVar(true)
@@ -22,13 +25,8 @@ const Guard = ({children}: GuardProps) => {
             snackVar(UNKNOWN_ERROR_SNACK_MESSAGE);
         }
     }, [error]);
-    return (
-        <>
-            {
-                excludedRoutes.includes(window.location.pathname) ? children : user && children
-            }
-        </>
-    );
+    
+    return <>{excludedRoutes.includes(path) ? children : user && children}</>;
 }
 
 export default Guard;
